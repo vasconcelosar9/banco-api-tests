8b548d273d7ac42bbdd473c62893d957bf0c7415
Alexandre Vasconcelos <arv.alexandre@gmail.com>
2025-09-26 10:54:04 -0300

    Adicionando fixtures para gerir os corpos das requisições

--- test/login.test.js
+++ test/login.test.js
@@ -1,17 +1,16 @@
 const request = require('supertest')
 const { expect } = require('chai')
 require('dotenv').config()
+const postLogin = require('../fixtures/postLogin.json')
 
 describe('Login', () => {
     describe('POST /login', () => {
         it('Deve retornar 200 com token em string quando usar credenciais válidas', async () => {
+            const bodyLogin = { ...postLogin }
             const res = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
-                .send({
-                    username: 'julio.lima',
-                    senha: '123456'
-                })
+                .send(bodyLogin)
             expect(res.status).to.equal(200)
             expect(res.body.token).to.be.a('string')
         })
--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,6 +1,8 @@
 const request = require('supertest')
 const { expect } = require('chai')
+require('dotenv').config()
 const { obterToken } = require('../helpers/autenticacao')
+const postTransferencias = require('../fixtures/postTransferencias.json')
 
 describe('Trasnferências', () => {
     describe('POST /transferencias', () => {
@@ -10,29 +12,22 @@ describe('Trasnferências', () => {
         })
 
         it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou maior que R$ 10,00', async () => {
+            const bodyTransferencias = { ...postTransferencias }
             const res = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    contaOrigem: 2,
-                    contaDestino: 1,
-                    valor: 10.00,
-                    token: ''
-                })
+                .send(bodyTransferencias)
             expect(res.status).to.equal(201)
         })
         it('Deve retornar falha com 422 quando o valor da transferência for menor que R$ 10,00', async () => {
-            const res = await request('http://localhost:3000')
+            const bodyTransferencias = { ...postTransferencias }
+            bodyTransferencias.valor = 9.99
+            const res = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    contaOrigem: 2,
-                    contaDestino: 1,
-                    valor: 9.99,
-                    token: ''
-                })
+                .send(bodyTransferencias)
             expect(res.status).to.equal(422)
         })
     })
